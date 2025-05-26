b15fff1a6aa49d88b8df74fe651c7058db017c3d
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-26 21:04:55 +0700

    add agregasi

--- agregasi.cpp
+++ agregasi.cpp
@@ -7,5 +7,14 @@ using namespace std;
 int main() { //agregasi
     ibu* varIbu = new ibu("dini");
     ibu* varIbu2 = new ibu("novi");
-    ibu*
+    ibu* varAnak1 = new anak("tono");
+    ibu* varAnak2 = new anak("rini");
+    ibu* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    
 }
