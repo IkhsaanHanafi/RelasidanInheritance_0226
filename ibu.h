c936e685f038c52f3d3c75f9ba83c93bc42eec7f
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-26 20:51:33 +0700

    add ibu

--- ibu.h
+++ ibu.h
@@ -8,6 +8,11 @@ public:
     vector<anak*> daftar_anak;
 
     ibu(string pNama) :nama(pNama) {
-        cout << "Ibu \" " << nama << "\" ada\n";
+        cout << "Ibu \"" << nama << "\" ada\n";
     }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
 }
