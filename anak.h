e0a4640365152392a2658a55b1f97c96e5c3dd77
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-26 20:43:33 +0700

    add anak

--- anak.h
+++ anak.h
@@ -1,4 +1,10 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-class anak
+class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \" " << nama << "\" ada\n";
+    }
+}
