1d22615ddedc729adf745b7a9b3d5594ede5664b
IkhsaanHanafi <ikhsaanhanafi@gmail.com>
2025-05-26 21:51:44 +0700

    last Inheritance

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -19,3 +19,26 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n
+\n"}
+};
+
+int main() {
+    pelajar siswa1("andi laksono", "SMAN ! Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
